f8bbd5cb1675c2301960ce1ea8e55ead53888570
Tranvu2704 <161577736+Tranvu2704@users.noreply.github.com>
2025-04-16 01:26:22 +0700

    edit

---
+++ Case Study 2/HRWebApp/HRWebApp/Controllers/GetPers.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using HRWebApp.Models;
+
+namespace HRWebApp.Controllers
+{
+    public class PersonalsApiController : Controller
+    {
+        private HRDB db = new HRDB();
+
+        // GET: /PersonalsApi/GetAll
+        public JsonResult GetAll()
+        {
+            var personals = db.Personals.Select(p => new
+            {
+                p.Employee_ID,
+                p.First_Name,
+                p.Last_Name,
+                p.Middle_Initial,
+                p.Address1,
+                p.Address2,
+                p.City,
+                p.State,
+                p.Zip,
+                p.Email,
+                p.Phone_Number,
+                p.Social_Security_Number,
+                p.Drivers_License,
+                p.Marital_Status,
+                p.Gender,
+                p.Shareholder_Status,
+                p.Benefit_Plans,
+                p.Ethnicity
+            }).ToList();
+
+            return Json(personals, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
